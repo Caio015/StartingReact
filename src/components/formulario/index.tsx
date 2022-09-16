4664b241e770d471a6a6cf27897f141e132f63cd
CaioPotenza <caio@petize.com.br>
2022-09-15 21:01:25 -0300

    inserindo css com scss

---
+++ src/components/formulario/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Botao from "../botao";
+import style from './Formulario.module.scss'
+
+class Formulario extends React.Component {
+
+    render(): React.ReactNode {
+        
+        return (
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
+                    <label htmlFor="tarefa">
+                        Adicione uma nova matéria
+                    </label>
+                    <input type= "text" name="tarefa" id="tarefa" placeholder= "O que você gostaria de estudar?" required></input>
+                </div>
+                <div className={style.inputContainer}>
+                    <label htmlFor="tempo">Tempo</label>
+                    <input type = "time" step= "1" name="tempo" id="tempo" min="00:00:00" max="01:30:00"></input>
+                </div>
+                <Botao></Botao>
+            </form>
+        )
+    }
+
+}
+
+export default Formulario 
